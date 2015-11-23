c480260f0ee5c04f0db0a39fba536758fba5afd3
Artem Malyshev <proofit404@gmail.com>
2015-11-23 23:57:14 +0300

    Install python infrastructure.
    - setuptools
    - pip
    - tox

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,3 +21,13 @@ apt-get install -y redis-server=3:3.0.5-1chl1~precise1
 # Install python 3.4
 
 apt-get install -y python3.4 python3.4-dev
+
+# Install setuptools and pip.
+
+wget https://bootstrap.pypa.io/ez_setup.py -O /tmp/ez_setup.py
+python3.4 /tmp/ez_setup.py
+easy_install-3.4 pip
+
+# Install tox.
+
+pip install tox
