ac64c7a3727b7227e81a7a0c9e408b4823d01167
Artem Malyshev <proofit404@gmail.com>
2015-11-23 23:20:09 +0300

    Register on PyPI.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='aiorq',
+    version='0.1.dev1',
+    description='asyncio client and server for RQ',
+    url='https://github.com/proofit404/aiorq',
+    license='LGPL-3',
+    author='Artem Malyshev',
+    author_email='proofit404@gmail.com')
