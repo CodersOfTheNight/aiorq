456dfbc1abf06e7de76e84b536589494115a32fa
Artem Malyshev <proofit404@gmail.com>
2015-11-23 17:34:42 +0300

    Use Ubuntu precise vagrant box.

--- Vagrantfile
+++ Vagrantfile
@@ -1,9 +1,8 @@
 Vagrant.configure(2) do |config|
-  config.vm.box = "puppetlabs/debian-7.8-32-nocm"
+  config.vm.box = "hashicorp/precise32"
 
   config.vm.provision "shell" do |script|
     script.path = "scripts/deploy.sh"
-    script.privileged = false
     script.keep_color = true
   end
 end
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,23 @@
-#!/bin/sh
+#!/bin/bash -e
 
-sudo sh -c 'echo "deb http://packages.dotdeb.org wheezy all" > /etc/apt/sources.list.d/dotdeb.list'
-sudo sh -c 'echo "deb-src http://packages.dotdeb.org wheezy all" >> /etc/apt/sources.list.d/dotdeb.list'
-wget --quiet -O - http://www.dotdeb.org/dotdeb.gpg | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install redis-server -y
+# Add deadsnakes mirror.
+
+echo "deb http://ppa.launchpad.net/fkrull/deadsnakes/ubuntu precise main" > /etc/apt/sources.list.d/python.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys DB82666C
+
+# Add redis mirror.
+
+echo "deb http://ppa.launchpad.net/chris-lea/redis-server/ubuntu precise main" > /etc/apt/sources.list.d/redis.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C7917B12
+
+# Update mirrors list.
+
+apt-get update
+
+# Install redis 3.0.5
+
+apt-get install -y redis-server=3:3.0.5-1chl1~precise1
+
+# Install python 3.4
+
+apt-get install -y python3.4 python3.4-dev
