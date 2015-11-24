ee66fe50b545021dd0345eb569994047632e67a9
Artem Malyshev <proofit404@gmail.com>
2015-11-25 00:47:20 +0300

    Add install requires.

--- setup.py
+++ setup.py
@@ -8,4 +8,5 @@ setup(
     license='LGPL-3',
     author='Artem Malyshev',
     author_email='proofit404@gmail.com',
-    packages=find_packages(exclude=['tests']))
+    packages=find_packages(exclude=['tests']),
+    install_requires=['rq', 'aioredis'])
