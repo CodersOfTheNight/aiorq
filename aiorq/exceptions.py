7202e5553a030ffb029c319af291b6f5b61ce059
Artem Malyshev <proofit404@gmail.com>
2015-12-02 00:19:44 +0300

    Import public rq exceptions into own module.

---
+++ aiorq/exceptions.py
@@ -0,0 +1,11 @@
+"""
+    aiorq.exceptions
+    ~~~~~~~~~~~~~~~~
+
+    This module imports all rq exceptions so you can use it through our api.
+
+    :copyright: (c) 2015 by Artem Malyshev.
+    :license: LGPL-3, see LICENSE for more details.
+"""
+
+from rq.exceptions import *     # noqa
