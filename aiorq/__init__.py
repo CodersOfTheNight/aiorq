da81bbecc4b47c6184ce8c2df8999e88667d69f0
Artem Malyshev <proofit404@gmail.com>
2015-12-03 15:44:44 +0300

    Make get_failed_queue public api.

--- aiorq/__init__.py
+++ aiorq/__init__.py
@@ -11,9 +11,9 @@
 from .connections import (Connection, pop_connection, push_connection,
                           get_current_connection, use_connection)
 from .job import get_current_job
-from .queue import Queue
+from .queue import get_failed_queue, Queue
 
 
 __all__ = ['Connection', 'pop_connection', 'push_connection',
            'get_current_connection', 'use_connection',
-           'get_current_job', 'Queue']
+           'get_current_job', 'get_failed_queue', 'Queue']
