122176ea463afd751d87cdb0f3d15d7f7d071ea3
Artem Malyshev <proofit404@gmail.com>
2015-12-14 21:48:47 +0300

    Test moving expired jobs to failed queue.

--- tests/test_registry.py
+++ tests/test_registry.py
@@ -1,4 +1,8 @@
+from rq.job import JobStatus
+
 from aiorq.job import Job
+from aiorq.queue import Queue, FailedQueue
+from fixtures import say_hello
 
 
 def test_add_and_remove(redis, registry, timestamp):
@@ -38,3 +42,25 @@ def test_get_expired_job_ids(redis, registry, timestamp):
     assert expired == ['foo']
     expired = yield from registry.get_expired_job_ids(timestamp + 20)
     assert expired == ['foo', 'bar']
+
+
+def test_cleanup(redis, registry):
+    """Moving expired jobs to FailedQueue."""
+
+    failed_queue = FailedQueue(connection=redis)
+    assert (yield from failed_queue.is_empty())
+
+    queue = Queue(connection=redis)
+    job = yield from queue.enqueue(say_hello)
+
+    yield from redis.zadd(registry.key, 2, job.id)
+
+    yield from registry.cleanup(1)
+    assert job.id not in (yield from failed_queue.job_ids)
+    assert (yield from redis.zscore(registry.key, job.id)) == 2
+
+    yield from registry.cleanup()
+    assert job.id in (yield from failed_queue.job_ids)
+    assert not (yield from redis.zscore(registry.key, job.id))
+    yield from job.refresh()
+    assert (yield from job.get_status()) == JobStatus.FAILED
