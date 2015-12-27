861b53e8baac1d836e53f5986160af856d64d411
Artem Malyshev <proofit404@gmail.com>
2015-12-27 21:49:35 +0300

    Add long package description and PyPI classifiers.

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
 from setuptools import setup, find_packages
 
+readme = open('README.rst').read() + open('CHANGELOG.rst').read()
+
 setup(
     name='aiorq',
     version='0.1.dev1',
     description='asyncio client and server for RQ',
+    long_description=readme,
     url='https://github.com/proofit404/aiorq',
     license='LGPL-3',
     author='Artem Malyshev',
     author_email='proofit404@gmail.com',
     packages=find_packages(),
-    install_requires=['rq', 'aioredis'])
+    install_requires=['rq', 'aioredis'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development',
+    ])
