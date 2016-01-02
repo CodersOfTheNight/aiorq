db0860c334c623e367804f460bc678b35cb596f8
Artem Malyshev <proofit404@gmail.com>
2016-01-02 22:24:39 +0300

    Add little delay into enqueueing example.

--- examples/enqueueing.py
+++ examples/enqueueing.py
@@ -11,9 +11,11 @@ def go():
     redis = yield from create_redis(('localhost', 6379))
     queue = Queue('my_queue', connection=redis)
     job = yield from queue.enqueue(mylib.add, 1, 2)
+    yield from asyncio.sleep(0.2)
     result = yield from job.result
     assert result == 3, '{!r} is not equal 3'.format(result)
     print('Well done, Turner!')
+    redis.close()
 
 
 loop = asyncio.get_event_loop()
