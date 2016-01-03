821bf0c2530756f3b92c3f952ba12957f8db6417
Artem Malyshev <proofit404@gmail.com>
2016-01-03 20:06:51 +0300

    Correct setup.py classifiers.

--- setup.py
+++ setup.py
@@ -17,11 +17,7 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Topic :: Software Development',
     ])
