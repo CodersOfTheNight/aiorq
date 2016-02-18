994d8cd393a7b30d57d9c183c818e6bb5ab15162
Artem Malyshev <proofit404@gmail.com>
2016-02-19 00:43:18 +0300

    Add dir locals.

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((python-mode
+  (python-shell-virtualenv-root . "/ssh:vagrant@127.0.0.1#2222:/home/vagrant/aiorq/.tox/py34/")))
