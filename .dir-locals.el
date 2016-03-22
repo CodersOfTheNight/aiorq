5235949301d104908e0f76412b5a167047a9c1cb
Artem Malyshev <proofit404@gmail.com>
2016-03-22 13:42:57 +0300

    Remove .dir-locals.el

--- .dir-locals.el
+++
@@ -1,5 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((python-mode
-  (python-shell-virtualenv-root . "/ssh:vagrant@127.0.0.1#2222:/home/vagrant/aiorq/.tox/py34/")))
