78d541d0c4fad554367838568bfebffa9bb86c7e
Artem Malyshev <proofit404@gmail.com>
2016-03-24 22:43:34 +0300

    Refactor worker process into functional test fixture.

--- func_tests/conftest.py
+++ func_tests/conftest.py
@@ -1,3 +1,9 @@
+import os
+import signal
+import subprocess
+import threading
+import time
+
 import pytest
 import redis
 
@@ -8,3 +14,84 @@ def flush_redis():
     yield
     connection = redis.StrictRedis()
     connection.flushdb()
+
+
+@pytest.fixture
+def worker():
+
+    return Worker()
+
+
+class Worker:
+
+    command = ['aiorq', 'worker', 'foo']
+    stop_interval = 0.5
+    kill_after = 300
+
+    def __init__(self):
+
+        self.process = None
+        self.is_running = False
+        self.start()
+        self.schedule_kill()
+
+    def start(self):
+
+        self.process = subprocess.Popen(
+            args=self.command, env=self.environment,
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        self.is_running = True
+
+    def stop_with(self, *signals):
+
+        self.schedule_stop(signals)
+        self.wait()
+        self.is_running = False
+
+    def schedule_stop(self, signals):
+
+        for generation, signum in enumerate(signals, 1):
+            timeout = self.stop_interval * generation
+            thread = threading.Thread(target=self.send, args=(signum, timeout))
+            thread.start()
+
+    def schedule_kill(self):
+
+        thread = threading.Thread(target=self.kill)
+        thread.start()
+
+    def wait(self):
+
+        stdout, stderr = self.process.communicate()
+        print(stdout.decode())
+        print(stderr.decode())
+
+    def send(self, signum, sleep_for):
+
+        time.sleep(sleep_for)
+        self.process.send_signal(signum)
+
+    def kill(self):
+
+        time.sleep(self.kill_after)
+        if self.is_running:
+            self.send(signal.SIGKILL, 0)
+
+    @property
+    def environment(self):
+
+        environment = os.environ.copy()
+        environment['PYTHONPATH'] = self.pythonpath
+        return environment
+
+    @property
+    def pythonpath(self):
+
+        return os.path.join(
+            os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+            'fixtures')
+
+    @property
+    def returncode(self):
+
+        return self.process.returncode
--- func_tests/test_signals.py
+++ func_tests/test_signals.py
@@ -1,9 +1,4 @@
-from os import environ
-from os.path import abspath, dirname, join
 from signal import SIGTERM
-from subprocess import Popen, PIPE
-from threading import Thread
-from time import sleep
 
 import pytest
 import rq
@@ -12,50 +7,17 @@ import rq
 pytestmark = pytest.mark.usefixtures('flush_redis')
 
 
-# Helpers.
-
-
-def run_worker(*, stop_with=None):
-    """Run aiorq worker instance listening the `foo` queue."""
-
-    cmd = ['aiorq', 'worker', 'foo']
-    fixtures = join(dirname(dirname(abspath(__file__))), 'fixtures')
-    environment = environ.copy()
-    environment['PYTHONPATH'] = fixtures
-    proc = Popen(cmd, stdout=PIPE, stderr=PIPE, env=environment)
-
-    for num, signum in enumerate(stop_with or [], 1):
-        timeout = 0.5 * num
-        Thread(target=kill_worker, args=(proc, signum, timeout))
-
-    stdout, stderr = proc.communicate()
-    print(stdout.decode())
-    print(stderr.decode())
-
-    return proc
-
-
-def kill_worker(process, signum, timeout):
-    """Wait for timeout and send signal to the process."""
-
-    sleep(timeout)
-    process.send_signal(signum)
-
-
-# Tests.
-
-
-def test_idle_worker_warm_shutdown():
+def test_idle_worker_warm_shutdown(worker):
     """Worker with no ongoing job receiving single SIGTERM signal and
     shutting down.
 
     """
 
-    proc = run_worker(stop_with=[SIGTERM])
-    assert proc.returncode == 0
+    worker.stop_with(SIGTERM)
+    assert worker.returncode == 0
 
 
-def test_working_worker_warm_shutdown():
+def test_working_worker_warm_shutdown(worker):
     """Worker with an ongoing job receiving single SIGTERM signal,
     allowing job to finish then shutting down.
 
@@ -66,13 +28,13 @@ def test_working_worker_warm_shutdown():
 
     job = queue.enqueue('fixtures.long_running_job')
 
-    run_worker(stop_with=[SIGTERM])
+    worker.stop_with(SIGTERM)
 
     assert job.is_finished
     assert 'Done sleeping...' == job.result
 
 
-def test_working_worker_cold_shutdown():
+def test_working_worker_cold_shutdown(worker):
     """Worker with an ongoing job receiving double SIGTERM signal and
     shutting down immediately.
 
@@ -83,6 +45,6 @@ def test_working_worker_cold_shutdown():
 
     job = queue.enqueue('fixtures.long_running_job')
 
-    run_worker(stop_with=[SIGTERM, SIGTERM])
+    worker.stop_with(SIGTERM, SIGTERM)
 
     assert not job.is_finished
